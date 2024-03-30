ca22e6c0c499b129bff97e6aff442ded34cca90f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-03-30 16:44:51 +0000

    Fix: Tabela ultrapassando o limite do Container

--- frontend/src/layouts/DefaultLayout/styles.ts
+++ frontend/src/layouts/DefaultLayout/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LayoutContainer = styled.div`
   background: ${(props) => props.theme['gray-800']};
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  min-height: calc(100vh - 10rem);
   margin: 5rem auto;
   padding: 2.5rem;
   border-radius: 8px;
