34769b872aef593cbcfa7541b737191a4baae328
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-03 08:06:39 +0100

    Contexto entre rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
+import { CycleContextProvider } from './contexts/CycleContext';
 import { Router } from './components/Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -8,7 +9,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CycleContextProvider>
+          <Router />
+        </CycleContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
